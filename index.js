d0ff167b50a33b1bc297d4339f94650492bd6e51
Enzosanchezz <enzosanchez771@gmail.com>
2023-02-02 22:17:41 -0300

    por favor andaaaa

--- index.js
+++ index.js
@@ -4,21 +4,21 @@
 //    ▒▒▒▒▒▒█▀▀█▀▀██▀██▒▒▒▒▒
 //    ▒▒▒▒▒▄█▒▄█▒▒▄█▒▄█▒▒▒▒▒
 //    ~~~~~~~~~~~~~~~~~~~~~~
-import { listen } from "./src/app.js";
-import { conn } from "./src/db.js";
-import { getPets } from "./src/tools/preCharge.js";
-import { getAdmin } from "./src/tools/preChargeAdmin.js";
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { getPets } = require("./src/tools/preCharge.js");
+const { getAdmin } = require("./src/tools/preChargeAdmin.js");
 
-import { getTestimonials } from "./src/tools/preChargeTestimonials.js";
+const { getTestimonials } = require("./src/tools/preChargeTestimonials.js");
 
 const { PORT } = process.env;
 
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   await getTestimonials();
   await getAdmin();
   await getPets();
 
-  listen(PORT || 7859, () => {
+  server.listen(PORT || 7859, () => {
 
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
