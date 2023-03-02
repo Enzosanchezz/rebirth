1702dbe9c85432fd3565d78e2584cf1a30d8bdee
Enzosanchezz <enzosanchez771@gmail.com>
2023-03-02 13:08:41 -0300

    aaaa

--- src/db.js
+++ src/db.js
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     timestamps: false,
+    dialect: 'postgres',
     dialectModule: pg,
     // dialectOptions: {
     //   ssl: {
