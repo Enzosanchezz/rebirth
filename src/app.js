ac793269cec3d50b6d3d2c4cbe4de1b8ab7e9013
Enzosanchezz <enzosanchez771@gmail.com>
2023-03-02 13:43:14 -0300

    set por use

--- src/app.js
+++ src/app.js
@@ -10,13 +10,13 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-server.use(express.json());
-server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req, res, next) => {
+server.set(express.json());
+server.set(cors());
+server.set(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.set(bodyParser.json({ limit: '50mb' }));
+server.set(cookieParser());
+server.set(morgan('dev'));
+server.set((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -24,15 +24,11 @@ server.use((req, res, next) => {
   next();
 });
 
-
-
-
-
 export default app;
-server.use('/', routes);
+server.set('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.set((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
