a895af88751060280d00e4444a8de09443270e5e
Enzosanchezz <enzosanchez771@gmail.com>
2023-03-03 12:53:42 -0300

    index js

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const { conn } = require("./src/db.js");
 const { getPets } = require("./src/tools/preCharge.js");
 const { getAdmin } = require("./src/tools/preChargeAdmin.js");
 const { getTestimonials } = require("./src/tools/preChargeTestimonials.js");
-// const { PORT } = process.env;
+const { PORT } = process.env;
 
 conn.sync({ force: false }).then(async () => {
   await getTestimonials();
   await getAdmin();
   await getPets();
   
-  server.listen(5432 || 7859, () => {
-    console.log(`%s listening at port`); 
+  server.listen(PORT, () => {
+    console.log(`%s listening at port ${PORT}`); 
   });
 });
